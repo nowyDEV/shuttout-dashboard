cbc82f7831040ea5d801341c7c905867f2b3ab7c
nowyTR <nowy.inheritance@gmail.com>
2017-08-01 15:36:56 +0200

    optional stats css fix

--- js/AdditionalStats.jsx
+++ js/AdditionalStats.jsx
@@ -3,10 +3,10 @@ import { Segment, Statistic } from 'semantic-ui-react';
 
 const AdditionalStats = props =>
   <Segment inverted>
-    <Statistic.Group widths="three">
-      <Statistic size='tiny' inverted value={parseFloat(props.exitRate).toFixed(2)} label="Exit Rate" />
-      <Statistic size='tiny' inverted color="yellow" value={parseFloat(props.bounceRate).toFixed(2)} label="Bounce Rate" />
-      <Statistic size='tiny' inverted color="teal" value={props.uniquePageviews} label="Unique Pageviews" />
+    <Statistic.Group size='small' widths="three">
+      <Statistic inverted value={parseFloat(props.exitRate).toFixed(2)} label="Exit Rate" />
+      <Statistic inverted color="yellow" value={parseFloat(props.bounceRate).toFixed(2)} label="Bounce Rate" />
+      <Statistic inverted color="teal" value={props.uniquePageviews} label="Unique Pageviews" />
     </Statistic.Group>
   </Segment>;
 
